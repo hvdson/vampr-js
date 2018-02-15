ea5e7c55ed38cbbd60d8c24f5b5a55185fe0c512
hvdson <hvdsonmusic@gmail.com>
2018-02-15 05:34:03 +0000

    finished exercise - no stretch:

--- vampire.js
+++ vampire.js
@@ -10,21 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let count = 0;
+    let currentVampire = this;
+    
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      count++;
+    }
+
+    return count;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
 
   }
 
@@ -36,6 +47,18 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let thisOGDistance = this.numberOfVampiresFromOriginal;
+    let vampireOGDistance = vampire.numberOfVampiresFromOriginal;
+
+    if (thisOGDistance === 0) {
+      return this;
+    } else if (vampireOGDistance === 0) {
+      return vampire;
+    } else {
+      
+    }
+
+
 
   }
 }
